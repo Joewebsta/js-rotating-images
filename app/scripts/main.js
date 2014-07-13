22e46efb50d9cc30c0606220ef7ae9c121172e82
Joe Webster <joseph.scott.webster@gmail.com>
2014-07-13 11:18:08 -0400

    First pass at changing background color in tandem with image rotation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
   var image = document.getElementsByTagName("img")[0];
+  var body = document.getElementsByTagName("body")[0];
   var imageSrc = ["images/beach.png", "images/road.png", "images/city.png", "images/mountains.png"];
   var imageSrcLength = imageSrc.length;
   var imageIndex = 0;
@@ -12,6 +13,24 @@ $(document).ready(function() {
 
     if ( imageIndex >= imageSrcLength ) {
       imageIndex = 0;
+    }
+
+    if ( imageIndex === 1 ) {
+      
+      body.setAttribute("class", "beach-bg-color");
+
+    } else if ( imageIndex === 2 ) {
+      
+      body.setAttribute("class", "road-bg-color");
+
+    } else if ( imageIndex === 3 ) {
+
+      body.setAttribute("class", "city-bg-color");
+
+    } else {
+
+      body.setAttribute("class", "mountain-bg-color");
+      
     }
   }
 
