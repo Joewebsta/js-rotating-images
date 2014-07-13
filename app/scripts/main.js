2036d74714b72ecd7ce11332a6cdf14e6854b4fe
Joe Webster <joseph.scott.webster@gmail.com>
2014-07-13 11:03:17 -0400

    Creates rotateImage function

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,24 +1,20 @@
-var data = {
-  "images" : [
-    {
-      "src": "images/",
-      "bgcolor" : "#FFF"
-    },
-    {
-      "src": "images/",
-      "bgcolor" : "#FFF"
-    },
-    {
-      "src": "images/",
-      "bgcolor" : "#FFF"
-    },
-    {
-      "src": "images/",
-      "bgcolor" : "#FFF"
-    },
-    {
-      "src": "images/",
-      "bgcolor" : "#FFF"
+$(document).ready(function() {
+
+  var image = document.getElementsByTagName("img")[0];
+  var imageSrc = ["images/beach.png", "images/road.png", "images/city.png", "images/mountains.png"];
+  var imageSrcLength = imageSrc.length;
+  var imageIndex = 0;
+
+  function rotateImage() {
+    
+    image.setAttribute("src" , imageSrc[imageIndex]);
+    imageIndex++;
+
+    if ( imageIndex >= imageSrcLength ) {
+      imageIndex = 0;
     }
-  ]
-}
+  }
+
+  setInterval(rotateImage, 3000);
+
+});
