3fc641f8e2b363d1242186f83c3b9f05a0a34f53
Joe Webster <joseph.scott.webster@gmail.com>
2014-07-13 12:34:47 -0400

    creates centerVertical function

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
 
   var image = document.getElementsByTagName("img")[0],
+      imageHeight = image.height,
       body = document.getElementsByTagName("body")[0],
       imageSrc = ["images/beach.png", "images/road.png", "images/city.png", "images/mountains.png"],
       imageSrcLength = imageSrc.length,
       bgColorClass = ["beach-bg-color", "road-bg-color", "city-bg-color", "mountain-bg-color"]
-      index = 0;
+      index = 0,
+      browserHeight = window.innerHeight;
 
   function rotateImage() {
     
@@ -18,6 +20,12 @@ $(document).ready(function() {
     } 
   }
 
-  setInterval(rotateImage, 3000);
+  function centerVertical() {
+    var marginTop = (browserHeight - (imageHeight + 20))/2;
+    image.setAttribute("style", "margin-top:" + marginTop + "px");
+  }
+
+  centerVertical();
+  // setInterval(rotateImage, 3000);
 
 });
